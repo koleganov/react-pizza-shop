5f9e29ef0d886a7fc09aaa8d3d09f3d207d00ba1
koleganov <taggz1030@gmail.com>
2023-03-13 16:34:32 +0500

    pagination finish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,7 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isloading ? skeletons : pizzas}</div>
-      <Pagination />
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
