b4f9fc61c4755a382bd5a59e50ad0cd92e8be422
koleganov <taggz1030@gmail.com>
2023-03-20 11:04:13 +0500

    optimized searchInput

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,29 +1,34 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setSort } from "../redux/slices/filterSlice";
 
-function Sort({ value, onChangeSort }) {
+function Sort() {
+  const dispatch = useDispatch();
+  const sort = useSelector((state) => state.filter.sort);
+
   const [open, setOpen] = React.useState(false);
 
   const list = [
     {
       id: 0,
       name: "популярности ↓",
-      sort: "rating",
+      sortProperty: "rating",
       order: "desc",
     },
     {
       id: 1,
       name: "популярности ↑",
-      sort: "rating",
+      sortProperty: "rating",
       order: "asc",
     },
-    { id: 2, name: "цене ↓", sort: "price", order: "desc" },
-    { id: 3, name: "цене ↑", sort: "price", order: "asc" },
-    { id: 4, name: "алфавиту ↓", sort: "title", order: "desc" },
-    { id: 5, name: "алфавиту ↑", sort: "title", order: "asc" },
+    { id: 2, name: "цене ↓", sortProperty: "price", order: "desc" },
+    { id: 3, name: "цене ↑", sortProperty: "price", order: "asc" },
+    { id: 4, name: "алфавиту ↓", sortProperty: "title", order: "desc" },
+    { id: 5, name: "алфавиту ↑", sortProperty: "title", order: "asc" },
   ];
 
-  const onClickListItem = (i) => {
-    onChangeSort(i);
+  const onClickListItem = (obj) => {
+    dispatch(setSort(obj));
     setOpen(false);
   };
 
@@ -43,7 +48,7 @@ function Sort({ value, onChangeSort }) {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span onClick={() => setOpen(!open)}>{value.name}</span>
+        <span onClick={() => setOpen(!open)}>{sort.name}</span>
       </div>
       {open && (
         <div className="sort__popup">
@@ -53,7 +58,7 @@ function Sort({ value, onChangeSort }) {
                 key={i}
                 onClick={() => onClickListItem(obj)}
                 className={
-                  value.sortProperty === obj.sortProperty ? "active" : ""
+                 sort.sortProperty === obj.sortProperty ? "active" : ""
                 }
               >
                 {obj.name}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import axios from "axios";
 import { SearchContext } from "../App";
 import { useSelector, useDispatch } from "react-redux";
 
-import {setCategoryId} from '../redux/slices/filterSlice'
+import { setCategoryId, setCurrentPage } from "../redux/slices/filterSlice";
 
 import Categories from "../components/Categories";
 import Sort from "../components/Sort";
@@ -12,41 +13,40 @@ import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 
 const Home = () => {
-  const categoryId = useSelector((state) => state.filter.categoryId);
   const dispatch = useDispatch();
+  const { categoryId, sort, currentPage } = useSelector(
+    (state) => state.filter
+  );
 
   const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isloading, setIsLoading] = React.useState(true);
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const [sortType, setSortType] = React.useState({
-    name: "популярности ↓",
-    sort: "rating",
-    order: "desc",
-  });
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
+  };
+
+  const onChangePage = (number) => {
+    dispatch(setCurrentPage(number));
   };
 
   React.useEffect(() => {
     setIsLoading(true);
     const search = searchValue ? `&search=${searchValue}` : "";
 
-    fetch(
-      `https://63ef5c5c4d5eb64db0c7c12b.mockapi.io/pizzas?page=${currentPage}&limit=4${
-        categoryId > 0 ? `category=${categoryId}` : ""
-      }&sortBy=${sortType.sort}&order=${sortType.order}${search}`
-    )
+    axios
+      .get(
+        `https://63ef5c5c4d5eb64db0c7c12b.mockapi.io/pizzas?page=${currentPage}&limit=4${
+          categoryId > 0 ? `category=${categoryId}` : ""
+        }&sortBy=${sort.sortProperty}&order=${sort.order}${search}`
+      )
       .then((res) => {
-        return res.json();
-      })
-      .then((arr) => {
-        setItems(arr);
+        setItems(res.data);
         setIsLoading(false);
       });
+
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, searchValue, currentPage]);
+  }, [categoryId, sort, searchValue, currentPage]);
 
   const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
@@ -58,11 +58,11 @@ const Home = () => {
     <div className="container">
       <div className="content__top">
         <Categories value={categoryId} onChangeCategory={onChangeCategory} />
-        <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
+        <Sort value={sort} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isloading ? skeletons : pizzas}</div>
-      <Pagination onChangePage={(number) => setCurrentPage(number)} />
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
 };
